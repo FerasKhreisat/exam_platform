331d83c2323d3322cefd07d6f3a2a5e1e3a206f9
Firas Khreasat <tecp085@gmail.com>
2025-11-30 23:12:20 +0300

    FORMAT: Clean up whitespace in config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-import os  
+import os
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///exam.db"  # احتياط للتشغيل المحلي فقط
+        "sqlite:///exam.db"   # احتياط للتشغيل المحلي فقط
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL", "admin@example.com")
